67094e35d987111a3ca8f769312afce506c8ce99
Diego A. Arturo A. <diegoarturoangulo30@gmail.com>
2024-09-02 17:48:14 -0500

    app no debug run

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def handle_message(data):
     emit('response', {'data': 'Mensaje recibido'})
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=True)
+    socketio.run(app, host='0.0.0.0')
